ee7662ba7cfedc202b5c292a502b5f6d26b1436a
“[bridgettenawari]” <[chebetbri@gmail.com]>
2026-06-29 14:21:03 +0300

    done

--- server/app.py
+++ server/app.py
@@ -6,5 +6,34 @@ contracts = [{"id": 1, "contract_information": "This contract is for John and bu
 customers = ["bob","bill","john","sarah"]
 app = Flask(__name__)
 
+@app.route('/contract/<int:id>')
+def contract(id):
+    for contract in contracts:
+        if contract['id'] == id:
+            return make_response(contract["contract_information"], 200)
+        else:
+            return make_response("Contract not found", 404)
+
+@app.route('/customer/<customer_name>')
+def customername(customer_name):
+    for customer in customers:
+        if customer_name in customer:
+            return make_response("", 204)
+        else:
+            return make_response("Customer not found", 404)
+        
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5000, debug=True)
+
+"""
+all_students = " ".join([f"<li>{student["name"]} <a href='/students/{id}'> View details </a></li>" for id,student in students.items() ])
+
+country = next((item for item in countries if item )
+return f' {country}'
+
+@app.route('/countries/<int:id>')
+def countries(id):
+    for country in countries:
+        if country["id"] == id:
+            return country
+"""
